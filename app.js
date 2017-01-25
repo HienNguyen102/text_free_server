406d4d6dcec82aa72ace28f00588026ca7f23779
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-01-24 21:39:34 -0800

    init socket io....

--- app.js
+++ app.js
@@ -3,23 +3,65 @@ var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io").listen(server);
 var fs = require("fs");
-server.listen(process.env.PORT || 3001);
+server.listen(process.env.PORT || 3000);
+
+var arrUsers = [];
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");	
 });
+
 io.sockets.on('connection', function (socket) {
-	
-  console.log("Co nguoi connect ne");
+  //console.log("Co nguoi connect ne - " + getDateTime());
+  socket.on('register_user', function (_userName, _email) {
+    var user = new User(_userName, _email);
+    if (arrUsers.indexOf(user)) {
+      arrUsers.push(user);
+      socket.un = _userName;
+      socket.emit('response_register', {retCode: 0, errorMsg:"SUCCESS", result:arrUsers});
+    }else{
+      socket.emit('response_register', {retCode: 1, errorMsg:"USER_ALREADY_EXITS", result: "USER_ALREADY_EXITS"});
+    }
+  	//io.sockets.emit('response_register', { noidung: data+" dang nhap ne" });
+  });
   
-  io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
+  //io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
   
-  socket.on('servernhantinnhan', function (data) {
+  /*socket.on('servernhantinnhan', function (data) {
 	// emit toi tat ca moi nguoi
 	io.sockets.emit('serverguitinnhan', { noidung: data });
 	
 	// emit tới máy nguoi vừa gửi
 	socket.emit('serverguitinnhan', { noidung: data });
   });
+  */
   
 });
+function User(_userName, _email){
+   this.userName = _userName;
+   this.email = _email;
+}
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
